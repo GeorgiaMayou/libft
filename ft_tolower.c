32f970c3503d7d1f7f78e3a3085e68224f002c92
Georgia MAYOU <gmayou@c3r1s4.wethinkcode.co.za>
2019-05-23 10:30:38 +0200

    change from upper or lower case

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmayou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 09:06:36 by gmayou            #+#    #+#             */
-/*   Updated: 2019/05/23 10:22:39 by gmayou           ###   ########.fr       */
+/*   Created: 2019/05/23 10:12:50 by gmayou            #+#    #+#             */
+/*   Updated: 2019/05/23 10:27:28 by gmayou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-
-int		ft_isalpha(int a);
-int		ft_isdigit(int a);
-int		ft_isalnum(int a);
-int		ft_isascii(int a);
-int		ft_isprint(int a);
-char	ft_toupper(char a);
-char	ft_tolower(char a);
-
-#endif
+char	ft_tolower(char a)
+{
+	if (a >= 'A' && a <= 'Z')
+	{
+		a = a + 32;
+		return (a);
+	}
+	else
+		return (a);
+}
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmayou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 09:06:36 by gmayou            #+#    #+#             */
-/*   Updated: 2019/05/23 10:22:39 by gmayou           ###   ########.fr       */
+/*   Created: 2019/05/23 09:35:37 by gmayou            #+#    #+#             */
+/*   Updated: 2019/05/23 10:02:56 by gmayou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-
-int		ft_isalpha(int a);
-int		ft_isdigit(int a);
-int		ft_isalnum(int a);
-int		ft_isascii(int a);
-int		ft_isprint(int a);
-char	ft_toupper(char a);
-char	ft_tolower(char a);
-
-#endif
+char	ft_toupper(char a)
+{
+	if (a >= 'a' && a <= 'z')
+	{
+		a = a - 32;
+		return (a);
+	}
+	else
+		return (a);
+}
