ea33f7fc841ee911bbdc29d6658467a2c8c96346
Georgia MAYOU <gmayou@c3r2s6.wethinkcode.co.za>
2019-05-24 13:53:00 +0200

    A to I

---
+++ ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmayou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 08:30:37 by gmayou            #+#    #+#             */
+/*   Updated: 2019/05/24 12:59:22 by gmayou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int ans;
+	int sign;
+
+	sign = 1;
+	ans = 0;
+	while (*str)
+	{
+		while (ft_isspace(*str) || *str == '+')
+			str++;
+		if ((*str == '+' || *str == '-') && (ft_isdigit(*(str + 1))))
+			if (*str == '-' && (sign = -1))
+				str++;
+		if (ft_isdigit(*str) == 1)
+		{
+			ans = *str - '0';
+			str++;
+			while (ft_isdigit(*str) == 1)
+			{
+				ans = (ans * 10) + (*str - '0');
+				str++;
+			}
+		}
+		return (ans * sign);
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gmayou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:06:36 by gmayou            #+#    #+#             */
-/*   Updated: 2019/05/23 18:06:47 by gmayou           ###   ########.fr       */
+/*   Updated: 2019/05/24 13:41:07 by gmayou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,7 @@ void    ft_putendl_fd(char const *str, int fd);
 size_t	ft_strlen(const char *s);
 char 	*ft_strcpy(char *dst, const char *src);
 char 	*ft_strdup(const char *str);
+int		ft_isspace(char c);
+int		ft_atoi(const char *str);
 
 #endif
